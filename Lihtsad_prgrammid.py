9ae1f2a536a2c12f8deb441841ff374aa46376c8
AnnaOleks <annaoleks88@gmal.com>
2024-11-27 16:47:42 +0200

    Praktiline töö: Lihtsad prgrammid

--- Lihtsad_prgrammid.py
+++ Lihtsad_prgrammid.py
@@ -2,6 +2,7 @@ from datetime import *
 from calendar import *
 from random import *
 from math import *
+from zlib import decompressobj
 
 
 #Ülesanne 1
@@ -16,7 +17,7 @@ print(d)
 print(m)
 print(y)
 
-novP=monthrange(2024,11)[1]
+novP=monthrange(2024,11)[1] #день недели первого дня месяца и количество дней в этом месяце
 jaaknov=novP-d
 
 detsP=monthrange(2024,12)[1]
@@ -24,28 +25,27 @@ jaakdets=novP+detsP-d
 
 print(f"Kuu lõppuni on {jaaknov} päeva")
 print(f"Aasta lõppuni on {jaakdets} päeva")
+print("\n ")
 
-\n
 #Ülesanne 2
 vastus1=3 + 8 / (4 - 2) * 4
 vastus2=3 + 8 / 4 - 2 * 4
 vastus3=(3 + 8) / (4 - 2) * 4
 print(vastus1,"\n",vastus2,"\n",vastus3)
+print("\n ")
 
 
 #Ülesanne 3
-#Ruudu sees asub ring. Ringi raadius on R.
-#Leia ja väljasta ekraanile ruudu pindala, ruudu ümbermõõt, ringi pindala, ringi ümbermõõt.
-try: 
+try: #помещается код, который может вызвать ошибку
     R=float(input("sisesta R: "))
     Sk=pi*R**2 #** - степень
     Lk=2*pi*R
     Skv=R*2**2
     Lkv=2*R*4
     print(f"Ringi pindala on {Sk}\nRingi ümbermõõt on {Lk}\nRuudu pindala on {Skv}\nRuudu ümbermõõt on {Lkv}")
-except:
+except: #действия, которые будут выполнены в случае возникновения ошибки
     print("On vaja number")
-
+print("\n ")
 
 R=int(random()*100) #генерирует значения от 0.0...1.0
 print(f"R={R}")
@@ -54,3 +54,19 @@ Lk=2*pi*R
 Skv=R*2**2
 Lkv=2*R*4
 print(f"Ringi pindala on {Sk}\nRingi ümbermõõt on {Lk}\nRuudu pindala on {Skv}\nRuudu ümbermõõt on {Lkv}")
+print("\n ")
+
+
+#Ülesanne 4
+maa=6378
+d=2.575
+maa*=100000 #maa=maa*100000
+Lmaa=2*pi*maa
+kogus=int(Lmaa/d)
+print(f"On vaja {kogus} mündi.\nMeil on vaja {kogus*2} eur")
+print("\n ")
+
+
+#Ülesanne 5
+kk="килл-колл ".capitalize()
+print(f"{kk*6}\n{kk}")
