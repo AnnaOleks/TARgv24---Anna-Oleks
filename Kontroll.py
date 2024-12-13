75fd6abe32161f1b31e2e6c8ff56ede9539ca5b6
AnnaOleks <annaoleks88@gmal.com>
2024-12-13 17:48:00 +0200

    06 - Kordused

---
+++ Kontroll.py
@@ -0,0 +1,14 @@
+﻿p=1
+for j in range(8):
+    while True:
+        try:
+            arv=float(input(f"Sisesta {j+1} arv: "))
+            break
+        except:
+            print("On vaja arv")
+    if arv>0: 
+        p*=arv
+    else:
+        print("Korrutame arvud rohkem kui 0")
+    print(f"{j+1}. samm korrutis= {p}")
+print(f"Lõpptulemus on {p}")
