2e66a4437acb9606a8d333dde0ef09c0872b71d7
AnnaOleks <annaoleks88@gmal.com>
2025-01-22 14:14:49 +0200

    15 - Def

---
+++ 15 - Def.py
@@ -0,0 +1,6 @@
+from Moodul import *
+a=int(input("Sisesta arv1: "))
+b=int(input("Sisesta arv2: "))
+c=input("Sisesta arv3: ")
+
+vastus=summa3(a,b,int(c))
---
+++ 15 - Moodul.py
@@ -0,0 +1,11 @@
+def summa3(arv1:int,arv2:int,arv3:int)->int:
+    """Tagastab kolme taisarvu summat
+
+    :param int arv1: Esimene number
+    :param int arv2: Teine number
+    :param int arv1: Kolmas number
+    :rtype: int
+
+    """
+    summa=arv1+arv2+arv3
+    return summa
