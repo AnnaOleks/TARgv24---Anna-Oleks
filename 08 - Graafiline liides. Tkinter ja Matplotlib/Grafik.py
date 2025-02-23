1d057a9e74205069f2e4e037085cb8da536f1fe4
AnnaOleks <annaoleks88@gmal.com>
2025-02-23 11:44:04 +0200

    Kvadraatilise võrrandi lahendamine

--- 08 - Graafiline liides. Tkinter ja Matplotlib/Grafik.py
+++ 08 - Graafiline liides. Tkinter ja Matplotlib/Grafik.py
@@ -6,7 +6,7 @@ def grafik(a,b,c):
     D=b**2-4*a*c
     x=np.linspace(-10, 10, 400)
     y=a*x**2+b*x+c
-    plt.plot(x,y,label=f"{a}x² + {b}x + {c}", color="slategray"+"-*")
+    plt.plot(x,y,label=f"{a}x² + {b}x + {c}", color="slategray", linestyle="-", marker="*", linewidth=0.5)
     if D>0:
         x1=(-b+np.sqrt(D))/(2 * a)
         x2=(-b-np.sqrt(D))/(2*a)
@@ -21,7 +21,7 @@ def grafik(a,b,c):
     plt.axvline(0, color='black',linewidth=0.5)
     plt.title(f"График функции {a}x² + {b}x + {c}")
     plt.xlabel("x")
-    plt.ylabel("f(x)")
+    plt.ylabel("y")
     plt.grid(True)
     plt.legend()
     
